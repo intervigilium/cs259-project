8974c30f3f9ea657b0a0da4906e55cdf7122dcfb
Ethan Chen <intervigil@gmail.com>
2011-03-10 18:37:53 -0800

    add twophase accelerator to main

--- src/main.c
+++ src/main.c
@@ -1,28 +1,30 @@
+#include "common.h"
 #include "gaussian_blur.h"
 #include "rician_deconv.h"
+#include "twophase.h"
 
 void main(double u1[M * N * P], double u2[M * N * P], double u3[M * N * P],
-	  double u4[M * N * P], double Ksigma, double sigma, double lambda)
+	  double u4[M * N * P], double in1, double in2, double in3)
 {
 	/* for deblur */
 	/*
-	   rician_deconv(u1, u2, u3, u4, Ksigma, sigma, lambda, 1);
+	   rician_deconv(u1, u2, u3, u4, in1, in2, in3, 1);
 	 */
 
 	/* for denoise */
 	/*
-	   rician_deconv(u1, u2, u3, u4, Ksigma, sigma, lambda, 0);
+	   rician_deconv(u1, u2, u3, u4, in1, in2, in3, 0);
 	 */
 
 	/* for registration */
 	/*
-	   gaussian_blur(u1, Ksigma);
-	   gaussian_blur(u2, Ksigma);
-	   gaussian_blur(u3, Ksigma);
+	   gaussian_blur(u1, in1);
+	   gaussian_blur(u2, in1);
+	   gaussian_blur(u3, in1);
 	 */
 
     /* for segmentation */
     /*
-
+    two_phase_3d_op_explicit(u1, u2, u3, in1, in2, in3);
     */
 }
