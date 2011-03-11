e315e0740bcba9eb977fb169f5aea33d1882698b
Ethan Chen <ethanc@cs.ucla.edu>
2011-03-10 21:21:58 -0800

    add select line to main accelerator interface

--- src/main.c
+++ src/main.c
@@ -4,33 +4,27 @@
 #include "twophase.h"
 
 int main(double u1[M * N * P], double u2[M * N * P], double u3[M * N * P],
-	 double u4[M * N * P], double in1, double in2, double in3)
+	 double u4[M * N * P], double in1, double in2, double in3, uint4_t select)
 {
 #pragma AP interface ap_bus port=u1 pipeline
 #pragma AP interface ap_bus port=u2 pipeline
 #pragma AP interface ap_bus port=u3 pipeline
 #pragma AP interface ap_bus port=u4 pipeline
-	/* for deblur */
-	/*
-	   rician_deconv(u1, u2, u3, u4, in1, in2, in3, 1);
-	 */
-
-	/* for denoise */
-	/*
-	   rician_deconv(u1, u2, u3, u4, in1, in2, in3, 0);
-	 */
-
-	/* for registration */
-	/*
-	   gaussian_blur(u1, in1);
-	   gaussian_blur(u2, in1);
-	   gaussian_blur(u3, in1);
-	 */
-
-	/* for segmentation */
-	
+	if (select == 0) {
+		/* for deblur */
+		rician_deconv(u1, u2, u3, u4, in1, in2, in3, 1);
+	} else if (select == 1) {
+		/* for denoise */
+		rician_deconv(u1, u2, u3, u4, in1, in2, in3, 0);
+	} else if (select == 2) {
+		/* for registration */
+		gaussian_blur(u1, in1);
+		gaussian_blur(u2, in1);
+		gaussian_blur(u3, in1);
+	} else {
+		/* for segmentation */
 	   two_phase_3d_op_explicit(u1, u2, u3, in1, in2, in3);
-	 
+	}
 
 	return 0;
 }
