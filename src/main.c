74a91e8f5376dd0db12754c36927c900a26c2571
Ethan Chen <intervigil@gmail.com>
2011-03-10 16:24:42 -0800

    implement FPGA main control module

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include "gaussian_blur.h"
 #include "rician_deconv.h"
 
-void main()
+void main(double u1[M][N][P], double u2[M][N][P], double u3[M][N][P],
+	  double u4[M][N][P], double Ksigma, double sigma, double lambda)
 {
+	/* for deblur */
+	/*
+	   rician_deconv(u1, u2, u3, u4, Ksigma, sigma, lambda, 1);
+	 */
 
+	/* for denoise */
+	/*
+	   rician_deconv(u1, u2, u3, u4, Ksigma, sigma, lambda, 0);
+	 */
+
+	/* for registration */
+	/*
+	   gaussian_blur(u1, Ksigma);
+	   gaussian_blur(u2, Ksigma);
+	   gaussian_blur(u3, Ksigma);
+	 */
 }
